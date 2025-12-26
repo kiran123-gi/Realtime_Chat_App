137d90a6607581f0d6d9ad10c5d3dbbc50a48d52
Pithani Sri Kiranmayi <182846695+kiran123-gi@users.noreply.github.com>
2025-12-26 19:30:55 +0530

    Add files via upload

---
+++ app/src/main/java/com/chat/app/config/WebSocketConfig.java
@@ -0,0 +1,33 @@
+package com.chat.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat")
+                .setAllowedOriginPatterns("http://localhost:8080/chat")
+                        .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> converters) {
+        converters.add(new MappingJackson2MessageConverter());
+        return false;
+    }
+}
